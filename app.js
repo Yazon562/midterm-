66cf7b8189cc1c863ea9e3d192e6b93349bdc5f4
Yazon Suleiman <yazonsuleiman@dhcp-39-2-165.dhcpint.csulb.edu>
2019-10-17 19:21:13 -0700

    test

--- app.js
+++ app.js
@@ -1,25 +1,75 @@
-function javaFunc ()
-{
-    var user = document.getElementById("username").value
-    var pass = document.getElementById("password").value
-    var conf = document.getElementById("confirmation").value
+function validateForm() {
 
-    var userL = user.length;
-    var passL = pass.length;
-    var conf = conf.length;
-    console.log(user);
-
-    if (userL >=4 && userL <=8 && passL >=4 && passL <=8  && confL >=4 && confL <=8  )
-    
-
-    {
-        alert("user another if funtion to compare password")
-    }
-
-    else{
-        alert("The length of user name and password must be between 4 and 8")
-    }       
-
-    
-
-}
+    var username = document.forms["myForm"]["Username"].value;
+    
+    var Password = document.forms["myForm"]["Password"].value;
+    
+    var confPassword = document.forms["myForm"]["confPassword"].value;
+    
+    if (username == "") {//validation for username
+    
+    document.getElementById("msg").innerHTML='Please username not empty<br>';
+    
+    return false;
+    
+    }
+    
+    if (Password == "") {//validation for password
+    
+    document.getElementById("msg").innerHTML='Please Password not empty<br>';
+    
+    return false;
+    
+    }
+    
+    if (confPassword == "") {//validation for Confriemd password
+    
+    document.getElementById("msg").innerHTML='Please Conform Password not empty<br>';
+    
+    return false;
+    
+    }
+    
+    if (username.length >8 || username.length < 4 ) {//validation for username
+    
+    document.getElementById("msg").innerHTML='User Name Must be between 4 and 8 character<br>';
+    
+    return false;
+    
+    }
+    
+    if (Password.length >8 || Password.length < 4 ) {//validation for password
+    
+    document.getElementById("msg").innerHTML='Password Must be between 4 and 8 character<br>';
+    
+    return false;
+    
+    }
+    
+    if (confPassword.length >8 || confPassword.length < 4 ) {//validation for Confriemd password
+    
+    document.getElementById("msg").innerHTML='Confirmed Password Must be between 4 and 8 character<br>';
+    
+    return false;
+    
+    }
+    
+    //validation for password and Confriemd password are same or not
+    
+    if(confPassword.localeCompare(Password)==0) {
+    
+    document.getElementById("msg").innerHTML="";
+    
+    document.getElementById("msg1").innerHTML='The input are correct';
+    
+    return false;}
+    
+    else{
+    
+    document.getElementById("msg").innerHTML='Password and Confirmed Password Not Same<br>';
+    
+    return false;}
+    
+    }
+    
+    
